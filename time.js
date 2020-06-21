297220ee829829d9b2c614055e68b68f604313c3
Manoj Chambarge <42155660+chambargemanoj@users.noreply.github.com>
2020-06-21 10:47:09 -0500

    Update time.js

--- time.js
+++ time.js
@@ -1,22 +1,36 @@
+var digitalClock = setInterval(headerTime, 1000)
 
-
-function headerDate () {
+function headerDate() {
   const options = {
     weekday: "long",
     year: "numeric",
-    month:"long",
-    day:"numeric"
-};
+    month: "long",
+    day: "numeric"
+  };
   const newdate = new Date().toLocaleDateString('en-US', options)
-  return newdate 
+  return newdate
 }
-function footerAllright () {
+
+function headerTime() {
+  var d = new Date()
+  var t = d.toLocaleTimeString('en-US')
+  var hour = d.getHours()
+  if (hour >= 0) {
+    hour = "Good Morning";
+  } else if (hour >= 12) {
+    hour = "Good Afternoon";
+  } else if (hour >= 18) {
+    hour = "Good Evening";
+  } else {
+    hour = "Invalid time, Something wrong in our code";
+  }
+  document.getElementById('time').innerHTML = t + " CST" + "  " + hour
+}
+
+function footerAllright() {
   const newdate = new Date()
-  year1  = newdate.getFullYear()
+  year1 = newdate.getFullYear()
   return year1
 }
-
-  const time = document.querySelector('#date').innerHTML=headerDate()
-  const year = document.querySelector('label.labelfooter').innerHTML='Copyright © ' + footerAllright()
-
-  
+document.querySelector('#date').innerHTML = headerDate()
+document.querySelector('label.labelfooter').innerHTML = 'Copyright © ' + footerAllright()
